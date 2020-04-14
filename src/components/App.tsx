0a846deeeb8fc228e573331c6b5b510934deaa46
Kel <kelstalker@hotmail.com>
2020-04-14 06:26:21 -0600

    No errors but lots of fixing still required

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/App.css';
-// import Login from './components/Login';
+import Login from './Login';
 import LoginUsingState from './LoginUsingState';
 import { IMenuItem } from '../models/menuItem';
 import NavBar from './Navbar';
 import { Fragment } from 'react';
+import MainSection from './MainSection';
+import SectionWithLoopRendering from './SectionWithLoopRendering';
+
 
 
 // function App() {
@@ -51,5 +54,47 @@ export default class App extends React.Component<IAppProps, IState> {
       isButtonPressed: false,
       userName: "default" 
     }
+  }
+
+  public onClickButton = () => {
+    this.setState({ isButtonPressed: true })
+  }
+
+  public onClickButtonToGoBack = () => {
+    this.setState({ isButtonPressed: false })
+  }
+//Try on your own: Using "normal" function syntax rather than fat arrow syntax.
+  //Notice that the error says "setState" doesn't exist.
+  public onClickUserNameSetButton = (newUserName: string) => {
+    console.log(this.state)
+    this.setState({ userName: newUserName });
+  }
+
+
+  public render() {
+    let { menuItems, isButtonPressed, userName } = this.state;
+    return (
+      // <Fragment>
+        <div className="App">
+
+
+          <NavBar
+            appName="Sample App"
+            isUserLoggedIn={false}
+            menuOptions={menuItems} />
+
+
+          <MainSection
+            isButtonPressed={isButtonPressed}
+            callbackMethod={this.onClickButton}
+            callbackMethodToGoBack={this.onClickButtonToGoBack}
+            userName={userName}
+            onClickUserNameSetButton={this.onClickUserNameSetButton} />
+
+          <SectionWithLoopRendering />
+
+        </div>
+      // </Fragment>
+    );
   }
 }
